ffc3bbbed8c2c39d18fd91b5002c9aab18521cbf
sulaiman-khan <sallokhan01999@gmail.com>
2023-07-12 16:57:58 +0500

    property-detail

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,23 +1,75 @@
-var wrapper = document.getElementById("wrapper");
-wrapper.classList.add("list"); // Add list class by default
+var sync1 = $(".slider");
+var sync2 = $(".navigation-thumbs");
 
-document.addEventListener("click", function (event) {
-  if (!event.target.matches(".list")) return;
+var thumbnailItemClass = '.owl-item';
 
-  // List view
-  event.preventDefault();
-  wrapper.classList.add("list");
+var slides = sync1.owlCarousel({
+	video:true,
+  startPosition: 12,
+  items:1,
+  loop:true,
+  margin:10,
+  autoplay:true,
+  autoplayTimeout:6000,
+  autoplayHoverPause:false,
+  nav: false,
+  dots: false,
+  
+}).on('changed.owl.carousel', syncPosition);
+
+function syncPosition(el) {
+  $owl_slider = $(this).data('owl.carousel');
+  var loop = $owl_slider.options.loop;
+
+  if(loop){
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    if(current < 0) {
+        current = count;
+    }
+    if(current > count) {
+        current = 0;
+    }
+  }else{
+    var current = el.item.index;
+  }
+
+  var owl_thumbnail = sync2.data('owl.carousel');
+  var itemClass = "." + owl_thumbnail.options.itemClass;
+
+
+  var thumbnailCurrentItem = sync2
+  .find(itemClass)
+  .removeClass("synced")
+  .eq(current);
+
+  thumbnailCurrentItem.addClass('synced');
+
+  if (!thumbnailCurrentItem.hasClass('active')) {
+    var duration = 300;
+    sync2.trigger('to.owl.carousel',[current, duration, true]);
+  }   
+}
+var thumbs = sync2.owlCarousel({
+  startPosition: 12,
+  items:4,
+  loop:false,
+  margin:10,
+  autoplay:false,
+  nav: false,
+  dots: false,
+  onInitialized: function (e) {
+    var thumbnailCurrentItem =  $(e.target).find(thumbnailItemClass).eq(this._current);
+    thumbnailCurrentItem.addClass('synced');
+  },
+})
+.on('click', thumbnailItemClass, function(e) {
+    e.preventDefault();
+    var duration = 300;
+    var itemIndex =  $(e.target).parents(thumbnailItemClass).index();
+    sync1.trigger('to.owl.carousel',[itemIndex, duration, true]);
+}).on("changed.owl.carousel", function (el) {
+  var number = el.item.index;
+  $owl_slider = sync1.data('owl.carousel');
+  $owl_slider.to(number, 100, true);
 });
-
-document.addEventListener("click", function (event) {
-  if (!event.target.matches(".grid")) return;
-
-  // Grid view
-  event.preventDefault();
-  wrapper.classList.remove("list");
-});
-
-
-// ===========================listing-pagination
-
-
